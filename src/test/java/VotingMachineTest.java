496300089a7efe9c88933ce046c1697659c4e6af
Ollara <oliverlacambra@gmail.com>
2017-01-19 20:13:39 +0100

    VotingMachineTest - canActive

---
+++ src/test/java/VotingMachineTest.java
@@ -0,0 +1,32 @@
+import kiosk.ActivationCard;
+import kiosk.VotingMachine;
+import org.junit.Assert;
+import org.junit.Test;
+import services.ValidationService;
+
+/**
+ * Created by Ollara on 19/01/2017.
+ */
+public class VotingMachineTest {
+
+    @Test
+    public void testCanActive() {
+        VotingMachine vm = new VotingMachine();
+        vm.setValidationService(new ValidationService() {
+            @Override
+            public boolean validate(ActivationCard card) {
+                return card.isActive();
+            }
+
+            @Override
+            public void deactivate(ActivationCard card) {
+                card.erase();
+            }
+        });
+
+        Assert.assertFalse(vm.canVote());
+        ActivationCard ac = new ActivationCard("ayyLmao");
+        vm.activateEmission(ac);
+        Assert.assertTrue(vm.canVote());
+    }
+}
